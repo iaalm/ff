84a32d9d15fb46742572935745f57c44560e6e8e
iaalm <iaalmsimon@gmail.com>
2014-03-27 22:29:59 +0800

    ring3 without interrupt

--- interrupt.h
+++ interrupt.h
@@ -2,6 +2,10 @@
 #define TIMER 0x20
 
 extern u64 idt[256];
+extern void timer_irq();
 extern void p();
 extern void pf();
+extern void gp();
+extern void df();
+extern void timer_irq();
 extern void udef_fun();
